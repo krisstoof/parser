30b61113357e11ed47c5d8760dd779f1cb37e5e7
Krzysztof Rzeszot <krz.rzeszot@gmail.com>
2020-01-16 17:56:30 +0100

    Add files via upload

--- product.php
+++ product.php
@@ -54,12 +54,12 @@ $html = file_get_html($url.'index.php?product='.$id);
 $list = $html->find('div.row', 0);
 $list_array = $list->find('div.col-lg-9');
 foreach($list_array as $element) {
-    $array['price'] = $element->find('div.card > div.card-body > h4.card-title > a[data-name]',0)->attr['data-name'];
-    $array['price_old'] = $url.$element->find('div.card > div.card-body > h4.card-title > a[data-name]',0)->href;
+    $array['price'] = $element->find('div.card > div.card-body > h5 > span.price")',0)->plaintext ? $element->find('div.card > div.card-body > h5 > span.price")',0)->plaintext : $element->find('div.card > div.card-body > h5 > span.price-promo")',0)->plaintext;
+    $array['price_old'] = $element->find('div.card > div.card-body > h5 > del")',0)->plaintext;
     $array['image'] = $element->find('div.card > a > img.card-img-top',0)->src;
     $array['code'] = $element->find('div.card > div.card-body > h5")',0)->children(1)->plaintext;
     // $array['description'] = $element->find('div.card > div.card-body > p.card-text")',0)->children(2)->plaintext;
-    $result = explode('(', $element->find('div.card > div.card-footer > small")',0)->plaintext);
+    $result = explode('(', $element->find('div.card > div.card-footer > small"',0)->plaintext);
     $array['stars'] = substr_count($result[0], '&#9733;');
     $array['amount'] = str_replace(')', '',$result[1]);
     
@@ -69,8 +69,11 @@ foreach($list_array as $element) {
 // foreach($array as $el) {
 //     fputcsv($file, $el, ';');
 // }
+echo '<br>';
+echo '<p>Gotowe</p>';
+echo '<button onclick="back()">Wstecz</button>';
+echo '<script>function back() {window.history.back()}</script>';
 
-echo 'Gotowe';
 
 
 // // fclose($file);
